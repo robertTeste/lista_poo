f41347fffa3d8cb4ba0a3cbf4ecb3e298ae3b4b3
robert <robert.geison100@gmail.com>
2026-03-28 17:13:46 -0300

    exercicio 4

--- src/exercicios/Carro.java
+++ src/exercicios/Carro.java
@@ -1,14 +1,27 @@
 package exercicios;
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
+
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public int getAno() { return ano; }
+
+    public void setAno(int ano) {
+        if (ano < 1886 || ano > 2026) {
+            System.out.println("Ano inválido!");
+        } else {
+            this.ano = ano;
+        }
+    }
 
     void exibir() {
         System.out.println(marca + " " + modelo + " " + ano);
-    }
-
-    int idadeDoCarro() {
-        return 2026 - ano;
     }
 }
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,28 +1,12 @@
 package exercicios;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Carro> lista = new ArrayList<>();
+        Carro c1 = new Carro();
+        c1.setMarca("Toyota");
+        c1.setModelo("Corolla");
+        c1.setAno(1885);
 
-        lista.add(new Carro());
-        lista.get(0).marca = "Toyota";
-        lista.get(0).modelo = "Corolla";
-        lista.get(0).ano = 2020;
-
-        lista.add(new Carro());
-        lista.get(1).marca = "Honda";
-        lista.get(1).modelo = "Civic";
-        lista.get(1).ano = 2018;
-
-        lista.add(new Carro());
-        lista.get(2).marca = "Ford";
-        lista.get(2).modelo = "Ka";
-        lista.get(2).ano = 2015;
-
-        for (Carro c : lista) {
-            c.exibir();
-        }
+        c1.exibir();
     }
 }
