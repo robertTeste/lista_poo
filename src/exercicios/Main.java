840a15a7915ae10b7358986a1242066c298cf26b
robert <robert.geison100@gmail.com>
2026-03-28 18:11:25 -0300

    exercicio 12

--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -31,7 +31,7 @@ public class Main {
         encontrá-l*/
 
 
-        int a = 10;
+        /* int a = 10;
         int b = a;
         b = 99;
 
@@ -39,9 +39,40 @@ public class Main {
         System.out.println("b = " + b);
 
 
-        /*
+
         a = 10
         b = 99
+        */
+
+        Carro c1 = new Carro("Fiat", "Uno", 2020);
+        Carro c2 = c1;
+
+        c2.setModelo("Palio");
+
+        /*
+a) Resultado no Console
+Ao rodar esse código, o resultado será:
+Modelo de c1: Palio
+Modelo de c2: Palio
+
+so acontece porque, em Java, quando você faz Carro c2 = c1;, você não está criando um novo carro. Você está criando uma nova "etiqueta" (referência) que aponta para o mesmo objeto que já existia na memória.
+
+O que eles compartilham?
+Eles compartilham o mesmo endereço de memória.
+Imagine que c1 é uma chave para uma casa.
+Ao fazer c2 = c1, você tirou uma cópia da chave.
+Agora, se você entrar na casa com a chave c2 e pintar a parede de azul,
+ quando você entrar com a chave c1,
+a parede também estará azul, porque a casa (o objeto) é a mesma.
+
+A diferença fundamental entre um int e um objeto Carro é onde os dados realmente moram e como o Java os copia:
+
+Exercício 11 (int - Valor): O dado vive na Stack. Ao fazer y = x, o Java cria uma cópia real do número.
+São duas caixas separadas; mexer em uma não afeta a outra.
+
+Exercício 12 (Carro - Referência): O objeto real vive na Heap. Na Stack, ficam apenas "controles remotos" (c1 e c2) que guardam o endereço do objeto.
+Ao fazer c2 = c1, você apenas deu o mesmo endereço para os dois.
+Como ambos apontam para o mesmo objeto na Heap, qualquer mudança por um reflete no outro.
         */
     }
 }
